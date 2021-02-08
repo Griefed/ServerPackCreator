b4cccf6b4def270fc976e541d3c0486b5784be43
Griefed <griefed@griefed.de>
2021-02-08 18:20:28 +0100

    - Exclude compile from gradle jar build - Add some strings to main so I know where the jar is at when executing

--- src/main/java/de/griefed/serverPackCreator/Main.java
+++ src/main/java/de/griefed/serverPackCreator/Main.java
@@ -7,10 +7,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //replace manual setting of modpack location by reading config file
         String packDir = "./Survive Create Prosper 4 1.16.5";
-
+        System.out.println("Copying Mods...");
         CopyFiles.copyMods(packDir);
+        System.out.println("Copying Configs...");
         CopyFiles.copyConfig(packDir);
+        System.out.println("Copying Scripts...");
         CopyFiles.copyScripts(packDir);
+        System.out.println("Copying Defaultconfigs...");
         CopyFiles.copyDefaultconfigs(packDir);
 
         System.out.println("All done!");
