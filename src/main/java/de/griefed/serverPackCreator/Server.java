20385446002200c322f7b877a8c69f0fa473e1d2
Griefed <griefed@griefed.de>
2021-02-13 21:13:08 +0100

    Setup deletion of client-side mods

--- src/main/java/de/griefed/serverPackCreator/Server.java
+++ src/main/java/de/griefed/serverPackCreator/Server.java
@@ -1,66 +1,21 @@
 package de.griefed.serverPackCreator;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
-public class ServerMods {
+public class Server {
     // deleting clientside mods from serverpack
-    public void deleteClientMods(String modpackDir, List<String> clientMods) {
-
-    }
-
-
-/*
-    public static List modsClient(String modDir) { // Create array with all mods inside mod directory to later compare against client side mods
-
-        UNCOMMENT LATER ON WHEN PATH IS PASSED BY MAIN AFTER READING CONFIG
-        String modDir = "./Survive Create Prosper 4 1.16.5/mods";
-
-        List<String> modsList = new ArrayList<String>(); // Create new list which will be filled with all mods in modpack
-        try {
-            File f = new File(modDir);
-            FilenameFilter filter = (f1, name) -> {
-                return name.endsWith(".jar"); // We want to find only .jar files in our mods directory
-            };
-            File[] files = f.listFiles(filter);
-            for (int i = 0; i < files.length; i++) {
-                    modsList.add(files[i].getName().toString());// Get the names of the files by using the .getName() method
+    public static void deleteClientMods(String modpackDir, List<String> clientMods) throws IOException {
+        System.out.println("Deleting client side mods...");
+        File serverMods = new File(modpackDir + "/server_pack/mods");
+            for (File f : Objects.requireNonNull(serverMods.listFiles())) {
+                for (int i = 0; i < clientMods.toArray().length; i++) {
+                    if (f.getName().startsWith(clientMods.get(i))) {
+                        f.delete();
+                }
             }
         }
-        catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-        return modsList;
     }
-
-    public boolean modsCheck(String modDir, String[] modsclient) {
-        File f = new File("C:\\");
-        File[] list = f.listFiles();
-        String[] mods = {
-                "AmbientSounds",
-                "BackTools",
-                "BetterAdvancement",
-                "BetterPing",
-                "cherished",
-                "ClientTweaks",
-                "Controlling",
-                "DefaultOptions",
-                "durability",
-                "DynamicSurroundings",
-                "itemzoom",
-                "jei-professions",
-                "jeiintegration",
-                "JustEnoughResources",
-                "MouseTweaks",
-                "Neat",
-                "OldJavaWarning",
-                "PackMenu",
-                "preciseblockplacing",
-                "SimpleDiscordRichPresence",
-                "SpawnerFix",
-                "TipTheScales",
-                "WorldNameRandomizer"
-        };
-        return modsCheck;
-    }
-*/
 }
