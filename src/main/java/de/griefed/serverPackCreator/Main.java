4c9af66992a09f078f6c040531663d440657523f
Griefed <griefed@griefed.de>
2021-02-13 19:28:01 +0100

    Initialize config system

--- src/main/java/de/griefed/serverPackCreator/Main.java
+++ src/main/java/de/griefed/serverPackCreator/Main.java
@@ -1,21 +1,10 @@
 package de.griefed.serverPackCreator;
 
-import java.io.IOException;
-
 public class Main {
+    // Go into copy and setup defaults, continue from there.
+    public static final Copy copy = new Copy();
 
-    public static void main(String[] args) throws IOException {
-        //replace manual setting of modpack location by reading config file
-        String packDir = "./Survive Create Prosper 4 1.16.5";
-        System.out.println("Copying Mods...");
-        CopyFiles.copyMods(packDir);
-        System.out.println("Copying Configs...");
-        CopyFiles.copyConfig(packDir);
-        System.out.println("Copying Scripts...");
-        CopyFiles.copyScripts(packDir);
-        System.out.println("Copying Defaultconfigs...");
-        CopyFiles.copyDefaultconfigs(packDir);
-
-        System.out.println("All done!");
+    public static void main(String[] args){
+        System.out.println("Done!");
     }
 }
