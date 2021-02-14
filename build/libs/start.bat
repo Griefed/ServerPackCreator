02ea039b33d72c485de7e99b8ae1c010dd4eb4dc
Griefed <griefed@griefed.de>
2021-02-14 09:29:08 +0100

    Upload alpha

---
+++ build/libs/start.bat
@@ -0,0 +1,5 @@
+
+
+java -Xmx8192M -Xms1024M -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=51 -XX:G1HeapRegionSize=32M -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar forge-1.16.5-36.0.15.jar nogui
+
+pause
