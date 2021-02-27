7e14b1f63b423e3a8a04f1858d0a99f895a55f0c
Griefed <griefed@griefed.de>
2021-02-27 18:15:46 +0100

    feat(fabric installer): Install fabric server and generate minecraft-server.jar

--- src/main/java/de/griefed/serverPackCreator/Server.java
+++ src/main/java/de/griefed/serverPackCreator/Server.java
@@ -1,16 +1,24 @@
 package de.griefed.serverPackCreator;
 
+import net.fabricmc.installer.server.ServerInstaller;
+import net.fabricmc.installer.util.InstallerProgress;
+import net.fabricmc.installer.util.LauncherMeta;
+import net.fabricmc.installer.util.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import static net.fabricmc.installer.util.InstallerProgress.CONSOLE;
 
 public class Server {
     private static final Logger appLogger = LogManager.getLogger("Server");
@@ -54,6 +62,48 @@ public class Server {
             outputStream.close();
         } catch (IOException ex) {
             errorLogger.error(ex);
+        }
+    }
+
+    public static void installServer(String modLoader, String modpackDir, String minecraftVersion, String modLoaderVersion) {
+        File serverDir = new File(modpackDir + "/server_pack");
+        if (modLoader.equals("Fabric")) {
+            try {
+                appLogger.info("################################################################");
+                appLogger.info("#                Starting Fabric installation                  #");
+                appLogger.info("################################################################");
+                // Feels like a dirty hack, but it works...
+                ServerInstaller.install(serverDir, modLoaderVersion, minecraftVersion, CONSOLE);
+                File serverJar = new File(serverDir, "server.jar");
+                File serverJarTmp = new File(serverDir, "server.jar.tmp");
+                Files.deleteIfExists((serverJar.toPath()));
+                InstallerProgress.CONSOLE.updateProgress((Utils.BUNDLE.getString("progress.download.minecraft")));
+                Utils.downloadFile(new URL(LauncherMeta.getLauncherMeta().getVersion(minecraftVersion).getVersionMeta().downloads.get("server").url), serverJarTmp.toPath());
+                Files.move(serverJarTmp.toPath(), serverJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                InstallerProgress.CONSOLE.updateProgress(Utils.BUNDLE.getString("progress.done"));
+                appLogger.info("################################################################");
+                appLogger.info("#         Fabric installation complete. Returning to SPC.      #");
+                appLogger.info("################################################################");
+            } catch (IOException ex) {
+                errorLogger.error("An error occurred during Fabric installation.", ex);
+            }
+        } else if (modLoader.equals("Forge")) {
+            appLogger.info("Forge installation not yet implemented.");
+            /*try {
+              appLogger.info("################################################################");
+                appLogger.info("#                Starting Forge installation                   #");
+                appLogger.info("################################################################");
+
+
+                appLogger.info("################################################################");
+                appLogger.info("#        Forge installation complete. Returning to SPC.        #");
+                appLogger.info("################################################################");
+            } catch (IOException ex) {
+                errorLogger.error("An error occurred during Forge installation.", ex);
+            }
+            */
+        } else {
+            errorLogger.error("Speficied invalid modloader: " + modLoader);
         }
     }
 }
