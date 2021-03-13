3acfdab08debf146150abe8486363db166159371
Griefed <griefed@griefed.de>
2021-03-13 21:48:55 +0100

    docs: Add documentation to methods, remove comments from within methods. Will be expanded upon in the future.

--- src/main/java/de/griefed/serverPackCreator/Main.java
+++ src/main/java/de/griefed/serverPackCreator/Main.java
@@ -26,7 +26,10 @@ public class Main {
     static Config conf;
 
     private static final Logger appLogger = LogManager.getLogger("Main");
-
+    /** Warn user about WIP status. Get configuration from serverpackcreator.conf. Check configuration. Print configuration. Make calls according to configuration.
+     * Basically, the main class makes the calls to every other class where the actual magic is happening. The main class of ServerPackCreator should never contain code which does work on the serverpack itself.
+     * @param args
+     */
     public static void main(String[] args) {
         appLogger.warn("################################################################");
         appLogger.warn("#         WORK IN PROGRESS! CONSIDER THIS ALPHA-STATE!         #");
@@ -34,29 +37,22 @@ public class Main {
         appLogger.warn("#         I CAN NOT BE HELD RESPONSIBLE FOR DATA LOSS!         #");
         appLogger.warn("#                    YOU HAVE BEEN WARNED!                     #");
         appLogger.warn("################################################################");
-        // Get name of the jar so log states version number. Good for issues on GitHub.
         try {
-            // Get path of the JAR file
             String jarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
             appLogger.info("JAR Path: " + jarPath);
-
-            // Get name of the JAR file
             String jarName = jarPath.substring(jarPath.lastIndexOf("/") + 1);
             appLogger.info("JAR Name: " + jarName);
 
         } catch (URISyntaxException ex) {
             appLogger.error("Error getting jar name.", ex);
         }
-        // Generate default files if they do not exist and exit if serverpackcreator.conf was created
         FilesSetup.filesSetup();
-        // Setup config variables with config file
         appLogger.info("Getting configuration...");
         conf = ConfigFactory.parseFile(FilesSetup.configFile);
         modpackDir = conf.getString("modpackDir");
         clientMods = conf.getStringList("clientMods");
         copyDirs = conf.getStringList("copyDirs");
         includeServerInstallation = conf.getBoolean("includeServerInstallation");
-        // Check whether Java is available if includeServerInstallation is true. Remove .exe-String if exists.
         if (includeServerInstallation && new File(conf.getString("javaPath")).exists()) {
             if (conf.getString("javaPath").endsWith(".exe")) {
                 javaPath = conf.getString("javaPath").substring(0, conf.getString("javaPath").length() - 4);
@@ -83,7 +79,6 @@ public class Main {
         includeServerProperties = conf.getBoolean("includeServerProperties");
         includeStartScripts = conf.getBoolean("includeStartScripts");
         includeZipCreation = conf.getBoolean("includeZipCreation");
-        // Print current configuration so in case of error, logs are more informative
         appLogger.info("Your configuration is:");
         appLogger.info("Modpack directory: " + modpackDir);
         appLogger.info("Client mods are:");
@@ -99,22 +94,21 @@ public class Main {
         appLogger.info("Include server properties:        " + includeServerProperties.toString());
         appLogger.info("Include start scripts:            " + includeStartScripts.toString());
         appLogger.info("Create zip-archive of serverpack: " + includeZipCreation.toString());
-        // Delete serverpack directory and .zip archive if they exist. Ensure clean state
+
         CopyFiles.cleanupEnvironment(modpackDir);
 
-        // Copy all specified directories from modpack to serverpack.
         try {
             CopyFiles.copyFiles(modpackDir, copyDirs);
         } catch (IOException ex) {
             appLogger.error("There was an error calling the copyFiles method.", ex);
         }
-        // Delete client-side mods from serverpack.
+
         try {
             ServerSetup.deleteClientMods(modpackDir, clientMods);
         } catch (IOException ex) {
             appLogger.error("There was an error calling the deleteClientMods method.", ex);
         }
-        // Generate Forge/Fabric start scripts and copy to serverpack.
+
         CopyFiles.copyStartScripts(modpackDir, modLoader, includeStartScripts);
 
         if (includeServerInstallation) {
@@ -122,19 +116,19 @@ public class Main {
         } else {
             appLogger.info("Not installing modded server.");
         }
-        // If true, copy server-icon to serverpack.
+
         if (includeServerIcon) {
             CopyFiles.copyIcon(modpackDir);
         } else {
             appLogger.info("Not including servericon.");
         }
-        // If true, copy server.properties to serverpack.
+
         if (includeServerProperties) {
             CopyFiles.copyProperties(modpackDir);
         } else {
             appLogger.info("Not including server.properties.");
         }
-        // If true, create zip archive of serverpack.
+
         if (includeZipCreation) {
             ServerSetup.zipBuilder(modpackDir, modLoader, minecraftVersion);
         } else {
