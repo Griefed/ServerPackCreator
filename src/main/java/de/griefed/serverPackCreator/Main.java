ab96e0796958557733bb5aa241f82cbf0c89a6eb
Mihail Yaremenko <m50421@outlook.com>
2021-03-15 19:03:32 +0200

    Added fabric version check, prepared isMinecraftVersionCorrect for CLI development.

--- src/main/java/de/griefed/serverPackCreator/Main.java
+++ src/main/java/de/griefed/serverPackCreator/Main.java
@@ -15,6 +15,8 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -40,7 +42,7 @@ public class Main {
      * Basically, the main class makes the calls to every other class where the actual magic is happening. The main class of ServerPackCreator should never contain code which does work on the serverpack itself.
      *
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         appLogger.warn("################################################################");
         appLogger.warn("#         WORK IN PROGRESS! CONSIDER THIS ALPHA-STATE!         #");
         appLogger.warn("#  USE AT YOUR OWN RISK! BE AWARE THAT DATA LOSS IS POSSIBLE!  #");
@@ -98,7 +100,7 @@ public class Main {
                 } catch (IOException ex) {
                     appLogger.error("Error: Couldn't create Minecraft Manifest JSON", ex);
                 }
-                if (!isMinecraftVersionCorrect()) {
+                if (!isMinecraftVersionCorrect(minecraftVersion)) {
                     configHasError = true;
                     appLogger.error("Error: Invalid Minecraft version specified.");
                 }
@@ -106,6 +108,9 @@ public class Main {
             if (!modLoader.equals("Forge") && !modLoader.equals("Fabric")) {
                 configHasError = true;
                 appLogger.error("Error: Incorrect mod loader specified.");
+            } else if (modLoader.equals("Fabric") && !isFabricVersionCorrect(modLoaderVersion)) {
+                configHasError = true;
+                appLogger.error("Error: Incorrect Fabric version specified.");
             }
             if (modLoaderVersion.equals("")) {
                 configHasError = true;
@@ -203,10 +208,10 @@ public class Main {
     }
     /** Optional. Check the specified Minecraft version against Mojang's version manifest to validate the version.
      *
-     * @return Returns boolean depending on whether the specified Minecraft version could be found in Mojang#s manifest.
+     * @return Returns boolean depending on whether the specified Minecraft version could be found in Mojang's manifest.
      */
 
-    private static boolean isMinecraftVersionCorrect() {
+    static boolean isMinecraftVersionCorrect(String mcver) {
         try {
             URL manifestJsonURL = new URL("https://launchermeta.mojang.com/mc/game/version_manifest.json");
             ReadableByteChannel readableByteChannel = Channels.newChannel(manifestJsonURL.openStream());
@@ -236,8 +241,58 @@ public class Main {
             String data = myReader.nextLine();
             myReader.close();
             data = data.replaceAll("\\s", "");
-            boolean contains = data.trim().contains(String.format("\"id\":\"%s\"", minecraftVersion));
+            boolean contains = data.trim().contains(String.format("\"id\":\"%s\"", mcver));
             manifestJsonFile.deleteOnExit();
+            return contains;
+        } catch (Exception ex) {
+            appLogger.error("An error occurred during Minecraft version validation.", ex);
+            return false;
+        }
+    }
+    /** Optional. Check the specified Minecraft version against Fabric's version manifest to validate the version.
+     *
+     * @return Returns boolean depending on whether the specified Minecraft version could be found in Fabric's manifest.
+     */
+
+    static boolean isFabricVersionCorrect(String version) {
+        try {
+            URL manifestJsonURL = new URL("https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml");
+            ReadableByteChannel readableByteChannel = Channels.newChannel(manifestJsonURL.openStream());
+            FileOutputStream downloadManifestOutputStream = null;
+            try {
+                downloadManifestOutputStream = new FileOutputStream("fabric-manifest.xml");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                File file = new File("fabric-manifest.xml");
+                if (!file.exists()){
+                    appLogger.info("Fabric Manifest XML File does not exist, creating...");
+                    boolean jsonCreated = file.createNewFile();
+                    if (jsonCreated) {
+                        appLogger.info("Fabric Manifest XML File created");
+                    } else {
+                        appLogger.error("Error. Could not create Fabric Manifest XML File.");
+                    }
+                }
+                downloadManifestOutputStream = new FileOutputStream("fabric-manifest.xml");
+            }
+            FileChannel downloadManifestOutputStreamChannel = downloadManifestOutputStream.getChannel();
+            downloadManifestOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            downloadManifestOutputStream.flush();
+            downloadManifestOutputStream.close();
+            File manifestXMLFile = new File("fabric-manifest.xml");
+            Scanner myReader = new Scanner(manifestXMLFile);
+            ArrayList<String> d = new ArrayList<>();
+            while (myReader.hasNextLine()) {
+                d.add(myReader.nextLine());
+            }
+            String[] data = new String[d.size()];
+            String manifestXML;
+            d.toArray(data);
+            manifestXML = Arrays.toString(data);
+            myReader.close();
+            manifestXML = manifestXML.replaceAll("\\s", "");
+            boolean contains = manifestXML.trim().contains(String.format("<version>%s</version>", version));
+            manifestXMLFile.deleteOnExit();
             return contains;
         } catch (Exception ex) {
             appLogger.error("An error occurred during Minecraft version validation.", ex);
