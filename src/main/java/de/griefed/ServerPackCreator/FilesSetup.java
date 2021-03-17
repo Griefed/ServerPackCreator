65c174b75200f99377406edb1260849fb5b64da2
Griefed <griefed@griefed.de>
2021-03-17 21:53:21 +0100

    refactor(config): Move file checks to separate methods for better separate checks

--- src/main/java/de/griefed/ServerPackCreator/FilesSetup.java
+++ src/main/java/de/griefed/ServerPackCreator/FilesSetup.java
@@ -18,18 +18,39 @@ class FilesSetup {
     static final File fabricWindowsFile = new File("start-fabric.bat");
     static final File fabricLinuxFile = new File("start-fabric.sh");
     private static final Logger appLogger = LogManager.getLogger("FilesSetup");
-    /** Generate all default files, including serverpackcreator.conf if it does not exist. Said default files provide an example for the user as to what is possible and a template as well.
-     * It is possible to run this application only with the serverpackcreator.conf, so long as the related config variables are set correctly.
-     * If any file was generated during execution of this method, it is considered as first run and will exit after generation of said conf-file and tell the user to customize and run again.
+    /**
+     * TODO: Write docs
      */
     static void filesSetup() {
         appLogger.info("Checking for default files...");
-        boolean firstRun = true;
         try {
             Files.createDirectories(Paths.get("./server_files"));
         } catch (IOException ex) {
             appLogger.error("Could not create server_files directory.", ex);
         }
+        if (checkForConfig()        ||
+            checkForFabricLinux()   ||
+            checkForFabricWindows() ||
+            checkForForgeLinux()    ||
+            checkForForgeWindows()  ||
+            checkForProperties()    ||
+            checkForIcon())         {
+                appLogger.warn("################################################################");
+                appLogger.warn("#             ONE OR MORE DEFAULT FILE(S) GENERATED.           #");
+                appLogger.warn("# CHECK THE LOGS TO FIND OUT WHICH FILE(S) WAS/WERE GENERATED. #");
+                appLogger.warn("#         CUSTOMIZE, THEN RUN SERVERPACKCREATOR AGAIN!         #");
+                appLogger.warn("################################################################");
+                System.exit(0);
+        } else {
+            appLogger.info("Setup completed.");
+        }
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForConfig() {
+        boolean firstRun = false;
         if (oldConfigFile.exists()) {
             try {
                 Files.copy(oldConfigFile.getAbsoluteFile().toPath(), configFile.getAbsoluteFile().toPath());
@@ -37,7 +58,6 @@ class FilesSetup {
                 if (isOldConfigDeleted) {
                     appLogger.info("creator.conf migrated to serverpackcreator.conf");
                 }
-                firstRun = false;
             } catch (IOException ex) {
                 appLogger.error("Error renaming creator.conf to serverpackcreator.conf", ex);
             }
@@ -46,101 +66,147 @@ class FilesSetup {
                 InputStream link = (CopyFiles.class.getResourceAsStream("/" + configFile.getName()));
                 Files.copy(link, configFile.getAbsoluteFile().toPath());
                 link.close();
-                firstRun = true;
                 appLogger.info("serverpackcreator.conf generated. Please customize.");
+                firstRun = true;
             } catch (IOException ex) {
                 if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
                     appLogger.error("Could not extract default config-file", ex);
+                    firstRun = true;
                 }
             }
-        } else { firstRun = false; }
-        if (!forgeWindowsFile.exists()) {
-            try {
-                InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + forgeWindowsFile.getName()));
-                Files.copy(link, Paths.get("./server_files/" + forgeWindowsFile));
-                link.close();
-                firstRun = true;
-                appLogger.info("start-forge.bat generated. Please customize if you intend on using it.");
-            } catch (IOException ex) {
-                if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
-                    appLogger.error("Could not extract default Forge Windows start file", ex);
-                }
-            }
-        } else { firstRun = false; }
-        if (!forgeLinuxFile.exists()) {
-            try {
-                InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + forgeLinuxFile.getName()));
-                Files.copy(link, Paths.get("./server_files/" + forgeLinuxFile));
-                link.close();
-                firstRun = true;
-                appLogger.info("start-forge.sh generated. Please customize if you intend on using it.");
-            } catch (IOException ex) {
-                if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
-                    appLogger.error("Could not extract default Forge Linux start file", ex);
-                }
-            }
-        } else { firstRun = false; }
-        if (!fabricWindowsFile.exists()) {
-            try {
-                InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + fabricWindowsFile.getName()));
-                Files.copy(link, Paths.get("./server_files/" + fabricWindowsFile));
-                link.close();
-                firstRun = true;
-                appLogger.info("start-fabric.bat generated. Please customize if you intend on using it.");
-            } catch (IOException ex) {
-                if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
-                    appLogger.error("Could not extract default Fabric Windows start file", ex);
-                }
-            }
-        } else { firstRun = false; }
+        }
+        return firstRun;
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForFabricLinux() {
+        boolean firstRun = false;
         if (!fabricLinuxFile.exists()) {
             try {
                 InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + fabricLinuxFile.getName()));
                 Files.copy(link, Paths.get("./server_files/" + fabricLinuxFile));
                 link.close();
-                firstRun = true;
                 appLogger.info("start-fabric.sh generated. Please customize if you intend on using it.");
+                firstRun = true;
             } catch (IOException ex) {
                 if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
                     appLogger.error("Could not extract default Fabric Linux start file", ex);
+                    firstRun = true;
                 }
             }
-        } else { firstRun = false; }
+        }
+        return firstRun;
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForFabricWindows() {
+        boolean firstRun = false;
+        if (!fabricWindowsFile.exists()) {
+            try {
+                InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + fabricWindowsFile.getName()));
+                Files.copy(link, Paths.get("./server_files/" + fabricWindowsFile));
+                link.close();
+                appLogger.info("start-fabric.bat generated. Please customize if you intend on using it.");
+                firstRun = true;
+            } catch (IOException ex) {
+                if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
+                    appLogger.error("Could not extract default Fabric Windows start file", ex);
+                    firstRun = true;
+                }
+            }
+        }
+        return firstRun;
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForForgeLinux() {
+        boolean firstRun = false;
+        if (!forgeLinuxFile.exists()) {
+            try {
+                InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + forgeLinuxFile.getName()));
+                Files.copy(link, Paths.get("./server_files/" + forgeLinuxFile));
+                link.close();
+                appLogger.info("start-forge.sh generated. Please customize if you intend on using it.");
+                firstRun = true;
+            } catch (IOException ex) {
+                if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
+                    appLogger.error("Could not extract default Forge Linux start file", ex);
+                    firstRun = true;
+                }
+            }
+        }
+        return firstRun;
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForForgeWindows() {
+        boolean firstRun = false;
+        if (!forgeWindowsFile.exists()) {
+            try {
+                InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + forgeWindowsFile.getName()));
+                Files.copy(link, Paths.get("./server_files/" + forgeWindowsFile));
+                link.close();
+                appLogger.info("start-forge.bat generated. Please customize if you intend on using it.");
+                firstRun = true;
+            } catch (IOException ex) {
+                if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
+                    appLogger.error("Could not extract default Forge Windows start file", ex);
+                    firstRun = true;
+                }
+            }
+        }
+        return firstRun;
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForProperties() {
+        boolean firstRun = false;
         if (!propertiesFile.exists()) {
             try {
                 InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + propertiesFile.getName()));
                 Files.copy(link, Paths.get("./server_files/" + propertiesFile));
                 link.close();
-                firstRun = true;
                 appLogger.info("server.properties generated. Please customize if you intend on using it.");
+                firstRun = true;
             } catch (IOException ex) {
                 if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
                     appLogger.error("Could not extract default server.properties file", ex);
+                    firstRun = true;
                 }
             }
-        } else { firstRun = false; }
+        }
+        return firstRun;
+    }
+    /** TODO: Write docs
+     *
+     * @return
+     */
+    private static boolean checkForIcon() {
+        boolean firstRun = false;
         if (!iconFile.exists()) {
             try {
                 InputStream link = (CopyFiles.class.getResourceAsStream("/server_files/" + iconFile.getName()));
                 Files.copy(link, Paths.get("./server_files/" + iconFile));
                 link.close();
-                firstRun = true;
                 appLogger.info("server-icon.png generated. Please customize if you intend on using it.");
+                firstRun = true;
             } catch (IOException ex) {
                 if (!ex.toString().startsWith("java.nio.file.FileAlreadyExistsException")) {
                     appLogger.error("Could not extract default server-icon.png file", ex);
+                    firstRun = true;
                 }
             }
-        } else { firstRun = false; }
-        if (firstRun) {
-            appLogger.warn("################################################################");
-            appLogger.warn("#             ONE OR MORE DEFAULT FILE(S) GENERATED.           #");
-            appLogger.warn("# CHECK THE LOGS TO FIND OUT WHICH FILE(S) WAS/WERE GENERATED. #");
-            appLogger.warn("#         CUSTOMIZE, THEN RUN SERVERPACKCREATOR AGAIN!         #");
-            appLogger.warn("################################################################");
-            System.exit(0);
-        } else {
-            appLogger.info("Setup completed.");
         }
+        return firstRun;
     }
 }
