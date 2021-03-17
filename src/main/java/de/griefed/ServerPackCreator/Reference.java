86a2b6a4c8c6edd2b966f5a23dcd6ab96f61ae2e
Griefed <griefed@griefed.de>
2021-03-17 21:56:18 +0100

    fix: Replace sout with logging to include config generation in log file

--- src/main/java/de/griefed/ServerPackCreator/Reference.java
+++ src/main/java/de/griefed/ServerPackCreator/Reference.java
@@ -1,8 +1,10 @@
 package de.griefed.ServerPackCreator;
 
 public class Reference {
-    public static final String MINECRAFT_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
-    public static final String FORGE_MANIFEST_URL = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/maven-metadata.json";
-    public static final String FABRIC_MANIFEST_URL = "https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml";
+    static final String MINECRAFT_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+    static final String FORGE_MANIFEST_URL = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/maven-metadata.json";
+    static final String FABRIC_MANIFEST_URL = "https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml";
     public static final String CONFIG_GEN_ARGUMENT = "-cgen";
+    /* TODO: Move file definitions from FilesSetup here to static final.  */
+    /* TODO: Check whether config variables from ConfigCheck can be moved here to static final. */
 }
