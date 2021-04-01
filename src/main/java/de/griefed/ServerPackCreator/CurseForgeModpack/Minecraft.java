79796671593082ed5dfabf090d5fab459b760347
Griefed <griefed@griefed.de>
2021-04-01 17:56:30 +0200

    feat(CurseForgeAPI): Gather information about the Minecraft version and modloader used by the modpack.

---
+++ src/main/java/de/griefed/ServerPackCreator/CurseForgeModpack/Minecraft.java
@@ -0,0 +1,32 @@
+package de.griefed.ServerPackCreator.CurseForgeModpack;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+public class Minecraft {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private String version;
+    private List<ModLoaders> modLoaders;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<ModLoaders> getModLoaders() {
+        return modLoaders;
+    }
+
+    public void setModLoaders(List<ModLoaders> modLoaders) {
+        this.modLoaders = modLoaders;
+    }
+
+    @Override
+    public String toString() {
+        return version + "," + modLoaders;
+    }
+}
