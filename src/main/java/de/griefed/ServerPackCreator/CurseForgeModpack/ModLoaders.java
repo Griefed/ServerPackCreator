3dd618f28af073b6c521c5ed32b74699944ebe6b
Griefed <griefed@griefed.de>
2021-04-01 17:56:53 +0200

    feat(CurseForgeAPI): Gather information about the modloader used by the modpack.

---
+++ src/main/java/de/griefed/ServerPackCreator/CurseForgeModpack/ModLoaders.java
@@ -0,0 +1,21 @@
+package de.griefed.ServerPackCreator.CurseForgeModpack;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class ModLoaders {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
