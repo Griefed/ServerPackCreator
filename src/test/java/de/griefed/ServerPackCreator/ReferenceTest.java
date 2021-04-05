c154017e092cedd404c6a819d045e468be168b03
Griefed <griefed@griefed.de>
2021-04-05 20:28:38 +0200

    test(JUnit): I mean....why not.

---
+++ src/test/java/de/griefed/ServerPackCreator/ReferenceTest.java
@@ -0,0 +1,43 @@
+package de.griefed.ServerPackCreator;
+
+import com.typesafe.config.Config;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+import java.util.List;
+
+class ReferenceTest {
+    @Mock
+    File oldConfigFile;
+    @Mock
+    File configFile;
+    @Mock
+    File propertiesFile;
+    @Mock
+    File iconFile;
+    @Mock
+    File forgeWindowsFile;
+    @Mock
+    File forgeLinuxFile;
+    @Mock
+    File fabricWindowsFile;
+    @Mock
+    File fabricLinuxFile;
+    @Mock
+    Config conf;
+    @Mock
+    List<String> clientMods;
+    @Mock
+    List<String> copyDirs;
+    @InjectMocks
+    Reference reference;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
