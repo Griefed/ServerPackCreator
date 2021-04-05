8597477dd4ea205e88a2a8d0981b84d4782483cd
Griefed <griefed@griefed.de>
2021-04-05 21:44:56 +0200

    test(JUnit): Write tests for CLISetup

---
+++ src/test/java/de/griefed/ServerPackCreator/CLISetupTest.java
@@ -0,0 +1,33 @@
+package de.griefed.ServerPackCreator;
+
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+class CLISetupTest {
+    @Mock
+    Logger appLogger;
+    @InjectMocks
+    CLISetup cLISetup;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testBuildString() {
+        List<String> args = Arrays.asList("config","mods","scripts","seeds","defaultconfigs");
+        String result = CLISetup.buildString(args.toString());
+        Assertions.assertEquals(args.toString(), String.format("[%s]",result));
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
