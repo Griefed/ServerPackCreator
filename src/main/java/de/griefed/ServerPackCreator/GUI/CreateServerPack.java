7a171c061606e2ff0bd040f8a04e666898b632de
Griefed <griefed@griefed.de>
2021-04-12 22:43:49 +0200

    feat(GUI): Some button and layout improvements

--- src/main/java/de/griefed/ServerPackCreator/GUI/CreateServerPack.java
+++ src/main/java/de/griefed/ServerPackCreator/GUI/CreateServerPack.java
@@ -10,6 +10,7 @@ public class CreateServerPack extends Component  {
         createServerPackPanel.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
 
+        //Labels and textinputs
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.weightx = 0.5;
 
@@ -18,13 +19,13 @@ public class CreateServerPack extends Component  {
         labelModpackDir.setToolTipText("Example: \"./Survive Create Prosper 4\" or CurseForge projectID,fileID combination 390331,3215793");
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.insets = new Insets(10,5,0,0);
+        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(labelModpackDir, constraints);
         JTextField textModpackDir = new JTextField("");
         textModpackDir.setToolTipText("Example: \"./Survive Create Prosper 4\" or CurseForge projectID,fileID combination 390331,3215793");
         constraints.gridx = 0;
         constraints.gridy = 1;
-        constraints.insets = new Insets(0,5,0,0);
+        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(textModpackDir, constraints);
 
         //Label and textfield clientMods
@@ -32,13 +33,13 @@ public class CreateServerPack extends Component  {
         labelClientMods.setToolTipText("Comma separated with no spaces. Example: AmbientSounds,BackTools,BetterAdvancement,BetterPing");
         constraints.gridx = 0;
         constraints.gridy = 3;
-        constraints.insets = new Insets(10,5,0,0);
+        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(labelClientMods, constraints);
         JTextField textClientMods = new JTextField("");
         textClientMods.setToolTipText("Comma separated with no spaces. Example: AmbientSounds,BackTools,BetterAdvancement,BetterPing");
         constraints.gridx = 0;
         constraints.gridy = 4;
-        constraints.insets = new Insets(0,5,0,0);
+        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(textClientMods, constraints);
 
         //Label and textfield javaPath
@@ -46,13 +47,13 @@ public class CreateServerPack extends Component  {
         labelJavaPath.setToolTipText("Must end with /java.exe or /java. Example: C:/Program Files/AdoptOpenJDK/jdk-8.0.275.1-hotspot/jre/bin/java.exe");
         constraints.gridx = 0;
         constraints.gridy = 6;
-        constraints.insets = new Insets(10,5,0,0);
+        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(labelJavaPath, constraints);
         JTextField textJavaPath = new JTextField("");
         textJavaPath.setToolTipText("Must end with /java.exe or /java. Example: C:/Program Files/AdoptOpenJDK/jdk-8.0.275.1-hotspot/jre/bin/java.exe");
         constraints.gridx = 0;
         constraints.gridy = 7;
-        constraints.insets = new Insets(0,5,0,0);
+        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(textJavaPath, constraints);
 
         //Label and textfield minecraftVersion
@@ -60,13 +61,13 @@ public class CreateServerPack extends Component  {
         labelMinecraftVersion.setToolTipText("Example: 1.16.5 or 1.12.2");
         constraints.gridx = 0;
         constraints.gridy = 9;
-        constraints.insets = new Insets(10,5,0,0);
+        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(labelMinecraftVersion, constraints);
         JTextField textMinecraftVersion = new JTextField("");
         textMinecraftVersion.setToolTipText("Example: 1.16.5 or 1.12.2");
         constraints.gridx = 0;
         constraints.gridy = 10;
-        constraints.insets = new Insets(0,5,0,0);
+        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(textMinecraftVersion, constraints);
 
         //Label and textfield Modloader
@@ -74,13 +75,13 @@ public class CreateServerPack extends Component  {
         labelModloader.setToolTipText("Must be either Forge or Fabric.");
         constraints.gridx = 0;
         constraints.gridy = 12;
-        constraints.insets = new Insets(10,5,0,0);
+        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(labelModloader, constraints);
         JTextField textModloader = new JTextField("");
         textModloader.setToolTipText("Must be either Forge or Fabric.");
         constraints.gridx = 0;
         constraints.gridy = 13;
-        constraints.insets = new Insets(0,5,0,0);
+        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(textModloader, constraints);
 
         //Label and textfield modloaderVersion
@@ -88,90 +89,85 @@ public class CreateServerPack extends Component  {
         labelModloaderVersion.setToolTipText("Example Forge: 36.1.4 - Example Fabric: 0.7.3");
         constraints.gridx = 0;
         constraints.gridy = 15;
-        constraints.insets = new Insets(10,5,0,0);
+        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(labelModloaderVersion, constraints);
         JTextField textModloaderVersion = new JTextField("");
         textModloaderVersion.setToolTipText("Example Forge: 36.1.4 - Example Fabric: 0.7.3");
         constraints.gridx = 0;
         constraints.gridy = 16;
-        constraints.insets = new Insets(0,5,0,0);
+        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(textModloaderVersion, constraints);
+
+        //Labels and checkboxes
+        constraints.insets = new Insets(10,10,0,0);
 
         //Label and checkBox installServer
         JLabel labelServer = new JLabel("Install modloader server in server pack?");
         labelServer.setToolTipText("Whether to install the server-software for the chosen modloader.");
         constraints.gridx = 0;
         constraints.gridy = 18;
-        constraints.insets = new Insets(10,5,0,0);
         createServerPackPanel.add(labelServer, constraints);
         JCheckBox checkBoxServer = new JCheckBox();
         constraints.gridx = 1;
         constraints.gridy = 18;
-        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(checkBoxServer, constraints);
 
-        //Label and checkBox installServer
+        //Label and checkBox copyIcon
         JLabel labelIcon = new JLabel("Include server-icon.png in server pack?");
         labelIcon.setToolTipText("Whether to copy the server-icon.png from server_files to the server pack.");
         constraints.gridx = 0;
         constraints.gridy = 20;
-        constraints.insets = new Insets(10,5,0,0);
         createServerPackPanel.add(labelIcon, constraints);
         JCheckBox checkBoxIcon = new JCheckBox();
         constraints.gridx = 1;
         constraints.gridy = 20;
-        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(checkBoxIcon, constraints);
 
-        //Label and checkBox installServer
+        //Label and checkBox copyProperties
         JLabel labelProperties = new JLabel("Include server.properties in server pack?");
         labelProperties.setToolTipText("Whether to copy the server.properties to the server pack.");
         constraints.gridx = 0;
         constraints.gridy = 22;
-        constraints.insets = new Insets(10,5,0,0);
         createServerPackPanel.add(labelProperties, constraints);
         JCheckBox checkBoxProperties = new JCheckBox();
         constraints.gridx = 1;
         constraints.gridy = 22;
-        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(checkBoxProperties, constraints);
 
-        //Label and checkBox installServer
+        //Label and checkBox copyStartScripts
         JLabel labelScripts = new JLabel("Include start scripts in server pack?");
         labelScripts.setToolTipText("Whether to copy the start scripts for the chosen modloader to the server pack.");
         constraints.gridx = 0;
         constraints.gridy = 24;
-        constraints.insets = new Insets(10,5,0,0);
         createServerPackPanel.add(labelScripts, constraints);
         JCheckBox checkBoxScripts = new JCheckBox();
         constraints.gridx = 1;
         constraints.gridy = 24;
-        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(checkBoxScripts, constraints);
 
-        //Label and checkBox installServer
+        //Label and checkBox createZIParchive
         JLabel labelZIP = new JLabel("Create ZIP-archive of server pack?");
         labelZIP.setToolTipText("Whether to create a ZIP-archive of the server pack for immediate upload.");
         constraints.gridx = 0;
         constraints.gridy = 26;
-        constraints.insets = new Insets(10,5,0,0);
         createServerPackPanel.add(labelZIP, constraints);
         JCheckBox checkBoxZIP = new JCheckBox();
         constraints.gridx = 1;
         constraints.gridy = 26;
-        constraints.insets = new Insets(10,10,0,0);
         createServerPackPanel.add(checkBoxZIP, constraints);
 
-        //Select modpackDir button
+        //Buttons
         constraints.anchor = GridBagConstraints.WEST;
         constraints.fill = GridBagConstraints.NONE;
+        constraints.insets = new Insets(0,10,0,0);
+
+        //Select modpackDir button
         JButton buttonModpackDir = new JButton();
         buttonModpackDir.setToolTipText("Open the file explorer to select the modpack directory.");
         buttonModpackDir.setIcon(ReferenceGUI.folderIcon);
         buttonModpackDir.setPreferredSize(ReferenceGUI.folderButtonDimension);
         constraints.gridx = 1;
         constraints.gridy = 1;
-        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(buttonModpackDir, constraints);
 
         //Select javaPath button
@@ -181,40 +177,40 @@ public class CreateServerPack extends Component  {
         buttonJavaPath.setPreferredSize(ReferenceGUI.folderButtonDimension);
         constraints.gridx = 1;
         constraints.gridy = 7;
-        constraints.insets = new Insets(0,10,0,0);
         createServerPackPanel.add(buttonJavaPath, constraints);
 
         //Load config from URL
         JButton buttonLoadConfigURL = new JButton();
-        buttonJavaPath.setToolTipText("Load a config from a URL. URL must be a direct link to a serverpackcreator.conf file.");
-        buttonJavaPath.setIcon(ReferenceGUI.loadIcon);
-        buttonJavaPath.setPreferredSize(ReferenceGUI.miscButtonDimension);
+        buttonLoadConfigURL.setToolTipText("Load a config from a URL. URL must be a direct link to a serverpackcreator.conf file.");
+        buttonLoadConfigURL.setIcon(ReferenceGUI.loadIcon);
+        buttonLoadConfigURL.setPreferredSize(ReferenceGUI.miscButtonDimension);
         constraints.anchor = GridBagConstraints.CENTER;
-        constraints.gridx = 3;
+        constraints.gridx = 2;
         constraints.gridy = 1;
-        constraints.gridwidth = 3;
         constraints.gridheight = 3;
         constraints.insets = new Insets(0,0,0,0);
-        createServerPackPanel.add(buttonJavaPath, constraints);
+        createServerPackPanel.add(buttonLoadConfigURL, constraints);
+
+
+        //Main action button
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.CENTER;
 
         //Start server pack generation button
-        constraints.fill = GridBagConstraints.HORIZONTAL;
         JButton buttonGenerateServerPack = new JButton();
         buttonGenerateServerPack.setToolTipText("Check the configuration for errors and start the generation of the server pack.");
         buttonGenerateServerPack.setIcon(ReferenceGUI.startGeneration);
         buttonGenerateServerPack.setPreferredSize(ReferenceGUI.folderButtonDimension);
-        constraints.anchor = GridBagConstraints.PAGE_END;
         constraints.gridwidth = 3;
-        constraints.gridheight = 1;
-        constraints.ipady = 50;
-        //constraints.ipadx = 50;
-        constraints.weighty = 1.0;
-        constraints.weightx = 1.0;
+        constraints.ipady = 70;
+        //constraints.weighty = 1.0;
+        //constraints.weightx = 1.0;
         constraints.gridx = 0;
-        constraints.gridy = 28;
-        constraints.insets = new Insets(20,50,20,50);
+        constraints.gridy = 27;
+        constraints.insets = new Insets(30,180,20,180);
         createServerPackPanel.add(buttonGenerateServerPack, constraints);
 
+        //Leftovers
         constraints.fill = GridBagConstraints.VERTICAL;
 
         return createServerPackPanel;
