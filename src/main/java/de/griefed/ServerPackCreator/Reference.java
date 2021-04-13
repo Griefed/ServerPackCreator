3fe48fbd6fdd67b9430f7b1a970952a843bb9a93
Mihail Yaremenko <m50421@outlook.com>
2021-04-13 11:51:24 +0300

    Made Reference class public
    In order to be able to access Reference.SUPPORTED_LANGUAGES from i18n package.

--- src/main/java/de/griefed/ServerPackCreator/Reference.java
+++ src/main/java/de/griefed/ServerPackCreator/Reference.java
@@ -4,7 +4,7 @@ import com.typesafe.config.Config;
 import java.io.File;
 import java.util.List;
 
-class Reference {
+public class Reference {
     static final String MINECRAFT_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
     static final String FORGE_MANIFEST_URL = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/maven-metadata.json";
     static final String FABRIC_MANIFEST_URL = "https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml";
