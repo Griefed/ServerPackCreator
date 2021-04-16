acb95019b0f33aa5e5e6e8725f854333fd4af1c4
Griefed <griefed@griefed.de>
2021-04-16 13:26:36 +0200

    feat(GUI): Add contributors to about

--- src/main/java/de/griefed/serverpackcreator/gui/About.java
+++ src/main/java/de/griefed/serverpackcreator/gui/About.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 
 public class About extends Component {
@@ -21,6 +22,50 @@ public class About extends Component {
         constraints.gridy = 0;
         constraints.gridwidth = 3;
 
+        //About Panel
+        JTextPane textPane = new JTextPane();
+        textPane.setEditable(false);
+        textPane.setOpaque(false);
+        textPane.setMinimumSize(new Dimension(getMaximumSize().width,520));
+        textPane.setPreferredSize(new Dimension(getMaximumSize().width,520));
+        textPane.setMaximumSize(new Dimension(getMaximumSize().width,520));
+
+        SimpleAttributeSet attributeSet = new SimpleAttributeSet();
+        StyleConstants.setBold(attributeSet, true);
+        StyleConstants.setFontSize(attributeSet, 14);
+        textPane.setCharacterAttributes(attributeSet, true);
+
+        StyledDocument document = textPane.getStyledDocument();
+        StyleConstants.setAlignment(attributeSet, StyleConstants.ALIGN_CENTER);
+        document.setParagraphAttributes(0, document.getLength(), attributeSet, false);
+
+        try {
+        document.insertString(
+                document.getLength(),
+                "ServerPackCreator was made as a training ground for Java \n" +
+                "and because I wanted an easier way to update/make server packs for updates to my modpack.\n" +
+                "It has grown substantially since the first release, which was when it was CLI only.\n" +
+                "Now it provides a GUI as well!\n" +
+                "The fact that this is, and always will be, a playground for me, in Java, still remains though. As with every application, bugs are bound to appear, and I will try to fix them whenever I can.\n" +
+                "I am working on this program in my spare time, please keep that in mind when reporting issues or requesting new features to be added.\n" +
+                "\n" +
+                "This project would not be where it is today without the help from my contributors:\n" +
+                "\n" +
+                "Whitebear60\n",
+                attributeSet
+        ); } catch (BadLocationException ex) {
+            appLogger.error("Error inserting string into document.", ex);
+        }
+
+        about.add(textPane, constraints);
+
+        //Buttons
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.insets = new Insets(15,10,10,10);
+        constraints.weightx = 0.1;
+        constraints.weighty = 0;
+        constraints.ipady = 40;
+        constraints.gridwidth = 1;
 
         //Button to upload log file to pastebin
         JButton buttonCheckForUpdate = new JButton();
