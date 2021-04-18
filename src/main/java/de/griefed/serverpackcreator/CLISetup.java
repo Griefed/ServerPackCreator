e55f44108bc2a7bdae95fe320d6f8da7907e540d
Griefed <griefed@griefed.de>
2021-04-18 17:12:05 +0200

    feat(GUI): Make public to allow for access from GUI. Other minor changes.

--- src/main/java/de/griefed/serverpackcreator/CLISetup.java
+++ src/main/java/de/griefed/serverpackcreator/CLISetup.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-class CLISetup {
+public class CLISetup {
     private static final Logger appLogger = LogManager.getLogger(CLISetup.class);
 
     /**
@@ -39,6 +39,7 @@ class CLISetup {
         Scanner reader = new Scanner(System.in);
         appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.start"), Reference.CONFIG_GEN_ARGUMENT));
         do {
+//--------------------------------------------------------------------------------------------MODPACK DIRECTORY---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.modpack.enter"));
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.modpack.example"));
 
@@ -51,6 +52,7 @@ class CLISetup {
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), modpackDir));
             System.out.println();
 
+//-----------------------------------------------------------------------------------------CLIENTSIDE-ONLY MODS---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.clientmods.enter"));
             do {
                 clientMods.addAll(readStringArray());
@@ -63,6 +65,7 @@ class CLISetup {
             clientMods.toArray(tmpClientMods);
             System.out.println();
 
+//---------------------------------------------------------------------------DIRECTORIES TO COPY TO SERVER PACK---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.copydirs.enter"));
             do {
                 do {
@@ -81,11 +84,13 @@ class CLISetup {
             copyDirs.toArray(tmpCopyDirs);
             System.out.println();
 
+//-------------------------------------------------------------WHETHER TO INCLUDE MODLOADER SERVER INSTALLATION---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.server.enter"));
             System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.server.include"));
             includeServerInstallation = readBoolean();
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), includeServerInstallation));
 
+//-------------------------------------------------------------------------------MINECRAFT VERSION MODPACK USES---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.minecraft.enter"));
             do {
                 System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.minecraft.specify"));
@@ -94,6 +99,7 @@ class CLISetup {
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), minecraftVersion));
             System.out.println();
 
+//---------------------------------------------------------------------------------------MODLOADER MODPACK USES---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.modloader.enter"));
             do {
                 System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.modloader.cli"));
@@ -103,6 +109,7 @@ class CLISetup {
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), modLoader));
             System.out.println();
 
+//----------------------------------------------------------------------------VERSION OF MODLOADER MODPACK USES---------
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.modloaderversion.enter"), modLoader));
             do {
                 System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.modloaderversion.cli"));
@@ -111,6 +118,7 @@ class CLISetup {
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), modLoaderVersion));
             System.out.println();
 
+//------------------------------------------------------------------------------------PATH TO JAVA INSTALLATION---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.java.enter"));
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.java.enter2"));
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.java.example"));
@@ -121,29 +129,34 @@ class CLISetup {
             } while (!Reference.configCheck.checkJavaPath(javaPath));
             System.out.println();
 
+//------------------------------------------------------------WHETHER TO INCLUDE SERVER-ICON.PNG IN SERVER PACK---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.icon.enter"));
             System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.icon.cli"));
             includeServerIcon = readBoolean();
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), includeServerIcon));
             System.out.println();
 
+//----------------------------------------------------------WHETHER TO INCLUDE SERVER.PROPERTIES IN SERVER PACK---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.properties.enter"));
             System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.properties.cli"));
             includeServerProperties = readBoolean();
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), includeServerProperties));
             System.out.println();
 
+//--------------------------------------------------------------WHETHER TO INCLUDE START SCRIPTS IN SERVER PACK---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.scripts.enter"));
             System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.scripts.cli"));
             includeStartScripts = readBoolean();
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), includeStartScripts));
             System.out.println();
 
+//----------------------------------------------------WHETHER TO INCLUDE CREATION OF ZIP-ARCHIVE OF SERVER PACK---------
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.zip.enter"));
             System.out.print(LocalizationManager.getLocalizedString("clisetup.log.info.zip.cli"));
             includeZipCreation = readBoolean();
             appLogger.info(String.format(LocalizationManager.getLocalizedString("clisetup.log.info.checkreturn"), includeZipCreation));
 
+//------------------------------------------------------------------------------PRINT CONFIG TO CONSOLE AND LOG---------
             Reference.configCheck.printConfig(modpackDir,
                     clientMods,
                     copyDirs,
@@ -161,6 +174,7 @@ class CLISetup {
         } while (!readBoolean());
         reader.close();
 
+//-----------------------------------------------------------------------------------------WRITE CONFIG TO FILE---------
         if (Reference.filesSetup.writeConfigToFile(
                 modpackDir,
                 buildString(Arrays.toString(tmpClientMods)),
@@ -173,7 +187,9 @@ class CLISetup {
                 includeServerIcon,
                 includeServerProperties,
                 includeStartScripts,
-                includeZipCreation
+                includeZipCreation,
+                Reference.configFile,
+                false
         )) {
             appLogger.info(LocalizationManager.getLocalizedString("clisetup.log.info.config.written"));
         }
@@ -200,7 +216,7 @@ class CLISetup {
      * @param args Strings that will be concatenated into one string
      * @return String. Returns concatenated string that contains all provided values.
      */
-    String buildString(String... args) {
+    public String buildString(String... args) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(Arrays.toString(args));
         stringBuilder.delete(0, 2).reverse().delete(0,2).reverse();
