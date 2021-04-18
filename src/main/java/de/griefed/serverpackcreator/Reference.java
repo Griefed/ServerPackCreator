afdfbe51c05483c2d201573b5f5f7feb968b2790
Griefed <griefed@griefed.de>
2021-04-18 17:09:28 +0200

    feat(GUI): Make various variables public to allow access from GUI.

--- src/main/java/de/griefed/serverpackcreator/Reference.java
+++ src/main/java/de/griefed/serverpackcreator/Reference.java
@@ -2,7 +2,7 @@ package de.griefed.serverpackcreator;
 
 import com.typesafe.config.Config;
 import de.griefed.serverpackcreator.curseforgemodpack.CreateModpack;
-import de.griefed.serverpackcreator.gui.InitGui;
+import de.griefed.serverpackcreator.gui.TabbedPane;
 
 import java.io.File;
 import java.util.List;
@@ -14,17 +14,18 @@ public class Reference {
 
     static final String CONFIG_GEN_ARGUMENT = "-cgen";
     static final String RUN_CLI_ARGUMENT    = "-cli";
-    static final String LANG_ARGUMENT = "-lang";
+    static final String LANG_ARGUMENT       = "-lang";
 
-    static final File oldConfigFile     = new File("creator.conf");
-    static final File configFile        = new File("serverpackcreator.conf");
+    public static final File configFile        = new File("serverpackcreator.conf");
+           static final File oldConfigFile     = new File("creator.conf");
+
 
     static final File propertiesFile    = new File("server.properties");
     static final File iconFile          = new File("server-icon.png");
     static final File forgeWindowsFile  = new File("start-forge.bat");
     static final File forgeLinuxFile    = new File("start-forge.sh");
     static final File fabricWindowsFile = new File("start-fabric.bat");
-    static final File fabricLinuxFile = new File("start-fabric.sh");
+    static final File fabricLinuxFile   = new File("start-fabric.sh");
 
     static final File langPropertiesFile = new File("lang.properties");
 
@@ -36,37 +37,37 @@ public class Reference {
 
     public static final CreateModpack createModpack = new CreateModpack();
 
-    static final InitGui initGui = new InitGui();
+    static final TabbedPane tabbedPane = new TabbedPane();
 
-    public static final FilesSetup filesSetup    = new FilesSetup();
-    static final Handler handler                 = new Handler();
-    static final CLISetup cliSetup               = new CLISetup();
-    static final ConfigCheck configCheck         = new ConfigCheck();
-    static final CopyFiles copyFiles             = new CopyFiles();
-    static final ServerSetup serverSetup         = new ServerSetup();
-    static final ServerUtilities serverUtilities = new ServerUtilities();
+    public static final FilesSetup      filesSetup      = new FilesSetup();
+    public static final CLISetup        cliSetup        = new CLISetup();
+    public static final ConfigCheck     configCheck     = new ConfigCheck();
+    public static final Handler         handler         = new Handler();
+           static final CopyFiles       copyFiles       = new CopyFiles();
+           static final ServerSetup     serverSetup     = new ServerSetup();
+           static final ServerUtilities serverUtilities = new ServerUtilities();
 
-    static Config config;
+    public static Config config;
 
-    static List<String>
+    public static List<String>
             clientMods,
             copyDirs;
 
-    static String
+    public static String
             modpackDir,
             javaPath,
             minecraftVersion,
             modLoader,
             modLoaderVersion;
 
-    static Boolean
+    public static Boolean
             includeServerInstallation,
             includeServerIcon,
             includeServerProperties,
             includeStartScripts,
             includeZipCreation;
 
-    static int
+    public static int
             projectID,
             projectFileID;
 }
