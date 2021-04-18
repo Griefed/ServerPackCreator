535062db579276be8bf41f6b83da52f383df8ea4
Griefed <griefed@griefed.de>
2021-04-18 17:10:01 +0200

    feat(GUI): Cleanup images. Add dimensions.

--- src/main/java/de/griefed/serverpackcreator/gui/ReferenceGUI.java
+++ src/main/java/de/griefed/serverpackcreator/gui/ReferenceGUI.java
@@ -7,19 +7,21 @@ import java.awt.*;
 import java.util.Objects;
 
 public class ReferenceGUI extends Reference {
-    static final ImageIcon folderIcon               = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/folder.png")));
-    static final ImageIcon serverPackCreatorIcon    = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/icon.png")));
-    static final ImageIcon startGeneration          = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/start_generation.png")));
-    static final ImageIcon bannerIcon               = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/banner.png")));
-    static final ImageIcon loadIcon                 = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/load.png")));
-    static final ImageIcon configIcon               = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/config.png")));
-    static final ImageIcon settingsIcon             = new ImageIcon(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/settings.png")));
+    static final ImageIcon folderIcon               = new ImageIcon(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/folder.png")));
+    static final ImageIcon startGeneration          = new ImageIcon(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/start_generation.png")));
+    static final ImageIcon bannerIcon               = new ImageIcon(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/banner.png")));
+    static final ImageIcon loadIcon                 = new ImageIcon(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/load.png")));
+    // static final ImageIcon configIcon               = new ImageIcon(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/config.png")));
+    // static final ImageIcon settingsIcon             = new ImageIcon(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/settings.png")));
 
-    static final Image icon = Toolkit.getDefaultToolkit().getImage(Objects.requireNonNull(InitGui.class.getResource("/de/griefed/resources/gui/app.png")));
+    static final Image icon = Toolkit.getDefaultToolkit().getImage(Objects.requireNonNull(TabbedPane.class.getResource("/de/griefed/resources/gui/app.png")));
 
     static final Color backgroundColour = new Color(255,248,235);
 
-    static final Dimension panelDimension           = new Dimension(600,800);
+    static final Dimension windowDimension          = new Dimension(800,860);
+    static final Dimension chooserDimension         = new Dimension(750,450);
+
     static final Dimension folderButtonDimension    = new Dimension(24,24);
     static final Dimension miscButtonDimension      = new Dimension(50,50);
+    static final Dimension startGenerationButton    = new Dimension(120,70);
 }
