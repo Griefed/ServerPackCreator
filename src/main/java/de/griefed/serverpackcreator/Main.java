21e8cc649599a0b9177b17bce55f7d96895bf2b6
Griefed <griefed@griefed.de>
2021-04-23 21:51:50 +0200

    other: Small update to main doc

--- src/main/java/de/griefed/serverpackcreator/Main.java
+++ src/main/java/de/griefed/serverpackcreator/Main.java
@@ -12,8 +12,7 @@ public class Main {
     private static final Logger appLogger = LogManager.getLogger(Main.class);
 
     /**
-     * Init and "main" has been moved to Handler-class. Main now only passes the cli args to Handler, which then runs the usual operations as they used to be in pre-2.x.x
-     * This had to be done so I could work in a non-static environment, which is required in some parts of the GUI.
+     * Init and "main" has been moved to Handler-class. Main now only inits the Localizationmanager and passes the cli args, if any,  to Handler, which then runs the usual operations as they used to be in pre-2.x.x
      * @param args Commandline arguments with which ServerPackCreator is run. Passed to Handler-class which then decides what to do corresponding to input.
      */
     public static void main(String[] args) {
