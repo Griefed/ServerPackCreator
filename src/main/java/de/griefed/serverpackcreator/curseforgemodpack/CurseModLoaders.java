a0e608f9bec7d4dcbc7940b7860f63421694e3e7
Griefed <griefed@griefed.de>
2021-05-02 11:52:41 +0200

    refactor(DI): Update javadocs

--- src/main/java/de/griefed/serverpackcreator/curseforgemodpack/CurseModLoaders.java
+++ src/main/java/de/griefed/serverpackcreator/curseforgemodpack/CurseModLoaders.java
@@ -17,23 +17,47 @@
  *
  * The full license can be found at https:github.com/Griefed/ServerPackCreator/blob/main/LICENSE
  */
-
 package de.griefed.serverpackcreator.curseforgemodpack;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//TODO: Write docs for class
+
+/**
+ *
+ */
 public class CurseModLoaders {
+    /**
+     * Ignore unknown values/object. We only want to gather specific information and disregard the rest. Setting this
+     * property allows us setup the class to only gather the information we want, so we don't have to worry about any
+     * additions to the source-data being made, which would otherwise cause an "Unknown property" exception.
+     */
     @JsonIgnoreProperties(ignoreUnknown = true)
     private String id;
 
+    /**
+     * Getter for the modloader and modloader version used by the CurseForge modpack. It's a single string in the
+     * manifest.json which contains the modloader and the version of said modloader, for example <code>"id": "forge-36.1.4"</code>
+     * @return String. Returns the id of the modpack acquired from CurseForge containing information about the modloader
+     * and the version of said modloader.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Setter for the modloader and modloader version used by the CurseForge modpack. It's a single string in the
+     * manifest.json which contains the modloader and the version of said modloader, for example <code>"id": "forge-36.1.4"</code>
+     * @param id Receives the id of the CurseForge modpack containing information about the modloader
+     * and the version of said modloader.
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * String containing information about the CurseForge modpack's used modloader and the modloader version.
+     * @return String. Returns the String with information about the modlaoder and modloader version used by the
+     * CurseForge modpack.
+     */
     @Override
     public String toString() {
         return id;
