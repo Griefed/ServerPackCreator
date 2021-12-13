efcb981f8962dd29085e4a542d640f3457e5f5da
Griefed <griefed@griefed.de>
2021-12-13 21:34:00 +0100

    no message

--- backend/test/java/de/griefed/serverpackcreator/MainSpringBootTest.java
+++ backend/test/java/de/griefed/serverpackcreator/MainSpringBootTest.java
@@ -23,11 +23,16 @@ import de.griefed.serverpackcreator.i18n.LocalizationManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import java.io.File;
 import java.io.IOException;
 
-@SpringBootTest
+@SpringBootTest(classes = MainSpringBootTest.class)
+@PropertySources({
+        @PropertySource("classpath:application.properties")
+})
 public class MainSpringBootTest {
 
     private final DefaultFiles DEFAULTFILES;
@@ -37,6 +42,12 @@ public class MainSpringBootTest {
     MainSpringBootTest() {
         try {
             FileUtils.copyFile(new File("backend/main/resources/serverpackcreator.properties"),new File("serverpackcreator.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileUtils.copyFile(new File("backend/test/resources/serverpackcreator.db"),new File("serverpackcreator.db"));
         } catch (IOException e) {
             e.printStackTrace();
         }
