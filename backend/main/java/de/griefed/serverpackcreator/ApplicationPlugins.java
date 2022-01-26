c3aec58be6c71d8d8be9dd88b419ffb65b2e7fd7
Griefed <griefed@griefed.de>
2022-01-26 18:15:26 +0100

    no message

--- backend/main/java/de/griefed/serverpackcreator/ApplicationPlugins.java
+++ backend/main/java/de/griefed/serverpackcreator/ApplicationPlugins.java
@@ -1,3 +1,22 @@
+/* Copyright (C) 2022  Griefed
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ *
+ * The full license can be found at https:github.com/Griefed/ServerPackCreator/blob/main/LICENSE
+ */
 package de.griefed.serverpackcreator;
 
 import de.griefed.serverpackcreator.plugins.serverpackhandler.PostGenExtension;
@@ -15,6 +34,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Manager for ServerPackCreator plugins. In itself it doesn't do much. It gathers lists of all available extensions for
+ * {@link TabExtension},{@link PreGenExtension},{@link PreZipExtension} and {@link PostGenExtension} so they can then be
+ * run during server pack generation and during initialization of the GUI.
+ * @author Griefed
+ */
 @Component
 public class ApplicationPlugins extends JarPluginManager {
 
