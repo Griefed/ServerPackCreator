dec6c71a04fec0f1154a16af0cc0684fa567bac8
Griefed <griefed@griefed.de>
2023-09-16 15:13:15 +0200

    fluff

--- buildSrc/src/main/kotlin/de/griefed/common/gradle/LicenseAgreementRenderer.kt
+++ buildSrc/src/main/kotlin/de/griefed/common/gradle/LicenseAgreementRenderer.kt
@@ -1,3 +1,22 @@
+/* Copyright (C) 2023  Griefed
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ *
+ * The full license can be found at https:github.com/Griefed/ServerPackCreator/blob/main/LICENSE
+ */
 package de.griefed.common.gradle
 
 import com.github.jk1.license.*
@@ -7,6 +26,12 @@ import org.gradle.api.Project
 import java.io.File
 import java.net.URL
 
+/**
+ * Custom made license renderer for https://github.com/jk1/Gradle-License-Report to generate a decent license
+ * agreement for use in installers.
+ *
+ * @author Griefed
+ */
 class LicenseAgreementRenderer(private val fileName: String = "LICENSE-AGREEMENT"): ReportRenderer {
 
     private lateinit var project: Project
