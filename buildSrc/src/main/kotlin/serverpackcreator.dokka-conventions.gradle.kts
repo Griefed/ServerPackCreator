631add06d567dbf6ea1eb45a0b3d7e2a608cadc1
Griefed <griefed@griefed.de>
2023-09-17 16:30:16 +0200

    Remove unnecessary path

--- buildSrc/src/main/kotlin/serverpackcreator.dokka-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/serverpackcreator.dokka-conventions.gradle.kts
@@ -33,7 +33,7 @@ tasks.withType<DokkaTask>().configureEach {
     }
 }
 
-val dokkaHtml by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class)
+val dokkaHtml by tasks.getting(DokkaTask::class)
 
 tasks.register<Jar>("dokkaJavadocJar") {
     dependsOn(tasks.dokkaHtml)
