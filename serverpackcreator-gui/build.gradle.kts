3dbec0e425ccb3ef912c36186c9165450039861c
Griefed <griefed@griefed.de>
2023-09-19 20:46:19 +0200

    build: Update GUI libraries and add additional fonts

--- serverpackcreator-gui/build.gradle.kts
+++ serverpackcreator-gui/build.gradle.kts
@@ -14,10 +14,13 @@ dependencies {
     implementation(project(":serverpackcreator-updater"))
 
     //New GUI
-    api("com.formdev:flatlaf:3.0")
-    api("com.formdev:flatlaf-extras:3.0")
-    api("com.formdev:flatlaf-intellij-themes:3.0")
+    api("com.formdev:flatlaf:3.2.1")
+    api("com.formdev:flatlaf-extras:3.2.1")
+    api("com.formdev:flatlaf-intellij-themes:3.2.1")
     api("com.formdev:flatlaf-fonts-jetbrains-mono:2.242")
+    api("com.formdev:flatlaf-fonts-inter:3.19")
+    api("com.formdev:flatlaf-fonts-roboto:2.137")
+    api("com.formdev:flatlaf-fonts-roboto-mono:3.000")
     api("com.miglayout:miglayout-swing:11.0")
     api("com.formdev:svgSalamander:1.1.4")
     api("net.java.balloontip:balloontip:1.2.4.1")
