df7dd1907861628f64abad6f15a88b48072d99c0
Griefed <griefed@griefed.de>
2023-11-24 21:25:07 +0100

    revert: Test back to 5.9.3, Kotlin Reflect to 1.9.10

--- serverpackcreator-web/build.gradle.kts
+++ serverpackcreator-web/build.gradle.kts
@@ -29,7 +29,7 @@ configurations {
 
 dependencies {
     api(project(":serverpackcreator-api"))
-    api("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+    api("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
     api("org.apache.activemq:artemis-jms-server:2.28.0")
     api("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     api("org.springframework.boot:spring-boot-starter-web:2.7.10")
@@ -38,8 +38,8 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-artemis:2.7.10")
     api("com.github.gwenn:sqlite-dialect:0.1.4")
     api("org.xerial:sqlite-jdbc:3.43.2.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.10")
     developmentOnly("org.springframework.boot:spring-boot-devtools:2.7.10")
 }
