32ce3e72930c8005ae906080cead0d4831e9a20c
Griefed <griefed@griefed.de>
2023-12-01 19:32:16 +0100

    fix: Remove modloader installer tab
    The installation feature got removed

--- serverpackcreator-gui/src/main/kotlin/de/griefed/serverpackcreator/gui/window/logs/TabbedLogsTab.kt
+++ serverpackcreator-gui/src/main/kotlin/de/griefed/serverpackcreator/gui/window/logs/TabbedLogsTab.kt
@@ -31,7 +31,6 @@ import de.griefed.serverpackcreator.gui.components.TabPanel
 class TabbedLogsTab(apiProperties: ApiProperties) : TabPanel() {
     init {
         tabs.addTab(Gui.createserverpack_gui_tabbedpane_serverpackcreatorlog_title.toString(), ServerPackCreatorLog(apiProperties))
-        tabs.addTab(Gui.createserverpack_gui_tabbedpane_modloaderinstallerlog_title.toString(), ModloaderInstallerLog(apiProperties))
         tabs.addTab(Gui.createserverpack_gui_tabbedpane_pluginshandlerlog_title.toString(), PluginsLog(apiProperties))
     }
 }
