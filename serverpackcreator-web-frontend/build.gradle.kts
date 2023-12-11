b9e052a170a3100639021081b9fcaf9c4adced84
Griefed <griefed@griefed.de>
2023-12-11 21:04:46 +0100

    build: I said include resources!

--- serverpackcreator-web-frontend/build.gradle.kts
+++ serverpackcreator-web-frontend/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     id("serverpackcreator.quasar-conventions")
 }
+
+tasks.register("copyDist", Copy::class) {
+    from(projectDir.resolve("dist/spa"))
+    into(rootDir.resolve("serverpackcreator-web/src/main/resources/static"))
+}
+
+tasks.build.get().finalizedBy(
+        tasks.getByName("copyDist")
+)
