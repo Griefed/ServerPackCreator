50acc54ccb27af58cc58c1141eb7fec239e037d4
Griefed <griefed@griefed.de>
2024-07-26 18:18:38 +0000

    Deploying to gh-pages from @ Griefed/ServerPackCreator@d508c51beedd5bb133a5201dcdd8fbc64afc835b 🚀

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":serverpackcreator-api:dokkaHtml/commonMain":[],":serverpackcreator-api:dokkaHtml/jvmMain":[":serverpackcreator-api:dokkaHtml/commonMain"]}'
+sourceset_dependencies='{":serverpackcreator-api:dokkaHtml/main":[]}'
