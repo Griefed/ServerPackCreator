be246c47fa286d3c5beab4e82c568a6bcd206549
Griefed <griefed@griefed.de>
2025-02-10 20:03:47 +0100

    chore: Add appleskin to whitelist to prevent automatic exclusion. Remove clickadv from clientside-mods list. Thanks to @Devilswarchild for the report!

--- serverpackcreator-api/src/main/kotlin/de/griefed/serverpackcreator/api/ApiProperties.kt
+++ serverpackcreator-api/src/main/kotlin/de/griefed/serverpackcreator/api/ApiProperties.kt
@@ -148,7 +148,8 @@ class ApiProperties(propertiesFile: File = File("serverpackcreator.properties"))
 
     private var fallbackModsWhitelist = TreeSet(
         listOf(
-            "Ping-Wheel-"
+            "Ping-Wheel-",
+            "appleskin-"
         )
     )
 
@@ -344,7 +345,6 @@ class ApiProperties(propertiesFile: File = File("serverpackcreator.properties"))
             "cirback-1.0-",                 //Gone? Reduces to atoms?
             "citresewn-",                   //https://www.curseforge.com/minecraft/mc-mods/forge-cit
             "classicbar-",                  //https://www.curseforge.com/minecraft/mc-mods/classic-bars
-            "clickadv-",                    //https://www.curseforge.com/minecraft/mc-mods/clickable-advancements
             "clienttweaks-",                //https://www.curseforge.com/minecraft/mc-mods/client-tweaks
             "combat_music-",                //https://www.curseforge.com/minecraft/mc-mods/combat-music
             "configured-neoforge-",         //https://www.curseforge.com/minecraft/mc-mods/configured
