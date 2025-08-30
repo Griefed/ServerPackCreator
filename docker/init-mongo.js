04914386b37240fc491c7119cef092cfe1f953d5
Griefed <griefed@griefed.de>
2025-08-30 17:10:42 +0200

    fix: Provide working init-mongo.js

--- docker/init-mongo.js
+++ docker/init-mongo.js
@@ -1,1 +1,12 @@
-db.getSiblingDB(process.env.MONGO_INITDB_DATABASE).createCollection(process.env.MONGO_INITDB_DATABASE);
+db.getSiblingDB('admin').auth(
+    process.env.MONGO_INITDB_ROOT_USERNAME,
+    process.env.MONGO_INITDB_ROOT_PASSWORD
+);
+
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE)
+db.createCollection(process.env.MONGO_INITDB_DATABASE)
+db.createUser({
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    roles: ["readWrite"],
+});
